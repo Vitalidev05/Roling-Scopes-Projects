00e4f3297eb88bbddd5eb84e8af5e98d84c78ead
Виталий <kantyshvitali@gmail.com>
2020-11-17 00:48:02 +0300

    feat: add constructor

--- gem-puzzle/src/js/gemPuzzle.js
+++ gem-puzzle/src/js/gemPuzzle.js
@@ -1,2 +1,20 @@
 export default class gemPuzzle {
+	constructor(size = 4, keySize = 75) {
+		//init variables
+		this.gameKeys = [];
+		this.counter = 0;
+		this.keySize = keySize;
+		this.random = [];
+		this.size = size;
+		this.gameArray = [];
+		this.sound;
+		this.empty;
+		this.popup;
+		this.newGame;
+		this.moves;
+		this.time;
+		//fill gameArray
+		this.fillArray(this.gameArray);
+	}
+ 
  }
