9f3d868016eff6253255b7fd1bee321ef7d8da68
Ilya-Baklanov <yahters4@gmail.com>
2020-12-22 02:50:03 +0400

    fix: resolve conflict

--- src/components/GraphCovid/GraphCovid.tsx
+++ src/components/GraphCovid/GraphCovid.tsx
@@ -6,9 +6,11 @@ import styles from '@/components/GraphCovid/GraphCovid.scss';
 import ComponentLayout from '../layout';
 
 const GraphCovid = (): JSX.Element => (
-  <div className={styles['graph-covid']}>
-    <Chart />
-  </div>
+  <ComponentLayout>
+    <div className={styles['graph-covid']}>
+      <Chart />
+    </div>
+  </ComponentLayout>
 );
 
 export default GraphCovid;
