af1ee1c00e486bb94f1ab3d8d594e92c650773a6
Ilya-Baklanov <yahters4@gmail.com>
2020-12-22 02:26:16 +0400

    feat: implemented a test chart

--- src/components/GraphCovid/Chart.tsx
+++ src/components/GraphCovid/Chart.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
+<<<<<<< HEAD
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import React, { useState, useEffect } from 'react';
 import { Pie } from 'react-chartjs-2';
@@ -9,6 +10,19 @@ const Chart = (): JSX.Element => {
   const [chartDataTotal, setChartDataTotal] = useState({});
 
   /*   function transformDate(date: string): string {
+=======
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable react/destructuring-assignment */
+import axios from 'axios';
+import React, { /* Component, */ useState, useEffect } from 'react';
+import { Line } from 'react-chartjs-2';
+
+const Chart = (): JSX.Element => {
+  const [chartData, setChartData] = useState({});
+
+  function transformDate(date: string): string {
+>>>>>>> e81a4cb... feat: implemented a test chart
     const options = {
       day: 'numeric',
       month: 'numeric',
@@ -16,6 +30,7 @@ const Chart = (): JSX.Element => {
     };
     const dateTransformed = new Date(date);
     return dateTransformed.toLocaleString('ru', options);
+<<<<<<< HEAD
   } */
 
   function fn1() {
@@ -62,11 +77,34 @@ const Chart = (): JSX.Element => {
                 ['rgba(200, 192, 192, 0.6)'],
                 ['rgba(230, 70, 192, 0.6)'],
               ],
+=======
+  }
+
+  const chart = () => {
+    const numberOfCases: [number] = [0];
+    const dateWithCases: [string] = [''];
+    axios
+      .get('https://api.covid19api.com/country/south-africa/status/confirmed')
+      .then(res => {
+        res.data.forEach((el: Record<string, number>) => numberOfCases.push(el['Cases']));
+        res.data.forEach((el: Record<string, string>) => {
+          const transformedDate = transformDate(el['Date']);
+          dateWithCases.push(transformedDate);
+        });
+        setChartData({
+          labels: dateWithCases,
+          datasets: [
+            {
+              label: 'test',
+              data: numberOfCases,
+              backgroundColor: ['rgba(75, 192, 192, 0.6)'],
+>>>>>>> e81a4cb... feat: implemented a test chart
               borderWidth: 4,
             },
           ],
         });
       })
+<<<<<<< HEAD
       .catch((error: Error) => setChartDataTotal({ error }));
   }
 
@@ -96,6 +134,47 @@ const Chart = (): JSX.Element => {
           options={{
             responsive: true,
             title: { text: 'Total cases', display: true },
+=======
+      .catch(err => {
+        // eslint-disable-next-line no-console
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    chart();
+  }, []);
+  return (
+    <div className="App">
+      <h1>Covid-cases</h1>
+      <div>
+        <Line
+          data={chartData}
+          options={{
+            responsive: true,
+            title: { text: 'TEST', display: true },
+            scales: {
+              yAxes: [
+                {
+                  ticks: {
+                    autoSkip: true,
+                    maxTicksLimit: 10,
+                    beginAtZero: true,
+                  },
+                  gridLines: {
+                    display: false,
+                  },
+                },
+              ],
+              xAxes: [
+                {
+                  gridLines: {
+                    display: false,
+                  },
+                },
+              ],
+            },
+>>>>>>> e81a4cb... feat: implemented a test chart
           }}
         />
       </div>
