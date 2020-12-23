18c17df0292deba78d51abcb86a17b3c6afd0c8e
Ilya-Baklanov <yahters4@gmail.com>
2020-12-23 04:53:20 +0400

    feat: add buttons and new states

--- src/components/GraphCovid/Chart.tsx
+++ src/components/GraphCovid/Chart.tsx
@@ -1,15 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable react/destructuring-assignment */
-import axios from 'axios';
 import React, { useState, useEffect } from 'react';
-import { Line } from 'react-chartjs-2';
+import { Pie } from 'react-chartjs-2';
+
+import styles from '@/components/GraphCovid/GraphCovid.scss';
 
 const Chart = (): JSX.Element => {
-  const [chartData, setChartData] = useState({});
+  const [chartDataTotal, setChartDataTotal] = useState({});
 
-  function transformDate(date: string): string {
+  /*   function transformDate(date: string): string {
     const options = {
       day: 'numeric',
       month: 'numeric',
@@ -17,70 +16,86 @@ const Chart = (): JSX.Element => {
     };
     const dateTransformed = new Date(date);
     return dateTransformed.toLocaleString('ru', options);
-  }
+  } */
 
-  const chart = () => {
-    const numberOfCases: [number] = [0];
-    const dateWithCases: [string] = [''];
-    axios
-      .get('https://api.covid19api.com/country/south-africa/status/confirmed')
+  function fn1() {
+    fetch('https://api.covid19api.com/summary')
+      .then(response => response.json())
       .then(res => {
-        res.data.forEach((el: Record<string, number>) => numberOfCases.push(el['Cases']));
-        res.data.forEach((el: Record<string, string>) => {
-          const transformedDate = transformDate(el['Date']);
-          dateWithCases.push(transformedDate);
-        });
-        setChartData({
-          labels: dateWithCases,
+        const dateWithCases = res.Global.TotalConfirmed;
+        const totalDeaths = res.Global.TotalDeaths;
+        const totalRecovered = res.Global.TotalRecovered;
+        setChartDataTotal({
+          labels: ['TotalConfirmed', 'TotalDeaths', 'TotalRecovered'],
           datasets: [
             {
               label: 'test',
-              data: numberOfCases,
-              backgroundColor: ['rgba(75, 192, 192, 0.6)'],
+              data: [dateWithCases, totalDeaths, totalRecovered],
+              backgroundColor: [
+                ['rgba(75, 192, 85, 0.6)'],
+                ['rgba(255, 192, 192, 0.6)'],
+                ['rgba(50, 192, 192, 0.6)'],
+              ],
               borderWidth: 4,
             },
           ],
         });
       })
-      .catch(err => {
-        // eslint-disable-next-line no-console
-        console.log(err);
-      });
-  };
+      .catch((error: Error) => setChartDataTotal({ error }));
+  }
+
+  function fn2() {
+    fetch('https://api.covid19api.com/summary')
+      .then(response => response.json())
+      .then(res => {
+        const numberOfCases = res.Global.NewConfirmed;
+        const newDeaths = res.Global.NewDeaths;
+        const newRecovered = res.Global.NewRecovered;
+        setChartDataTotal({
+          labels: ['NewConfirmed', 'NewDeaths', 'NewRecovered'],
+          datasets: [
+            {
+              label: 'test',
+              data: [numberOfCases, newDeaths, newRecovered],
+              backgroundColor: [
+                ['rgba(150, 90, 192, 0.6)'],
+                ['rgba(200, 192, 192, 0.6)'],
+                ['rgba(230, 70, 192, 0.6)'],
+              ],
+              borderWidth: 4,
+            },
+          ],
+        });
+      })
+      .catch((error: Error) => setChartDataTotal({ error }));
+  }
 
   useEffect(() => {
-    chart();
+    fn1();
   }, []);
+
   return (
-    <div className="App">
+    <div>
       <h1>Covid-cases</h1>
+      <div className={styles['nav-bar-wrapper']}>
+        <div className={styles['nav-bar']}>
+          <button className={styles['nav-bar__button']} type="button" onClick={() => fn1()}>
+            One
+          </button>
+          <button className={styles['nav-bar__button']} type="button" onClick={() => fn2()}>
+            Two
+          </button>
+          <button className={styles['nav-bar__button']} type="button">
+            Three
+          </button>
+        </div>
+      </div>
       <div>
-        <Line
-          data={chartData}
+        <Pie
+          data={chartDataTotal}
           options={{
             responsive: true,
-            title: { text: 'TEST', display: true },
-            scales: {
-              yAxes: [
-                {
-                  ticks: {
-                    autoSkip: true,
-                    maxTicksLimit: 10,
-                    beginAtZero: true,
-                  },
-                  gridLines: {
-                    display: false,
-                  },
-                },
-              ],
-              xAxes: [
-                {
-                  gridLines: {
-                    display: false,
-                  },
-                },
-              ],
-            },
+            title: { text: 'Total cases', display: true },
           }}
         />
       </div>
