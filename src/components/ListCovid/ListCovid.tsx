182ecf9a8c8cdbaf5ce844c6bf3e132922f765d0
Виталий <kantyshvitali@gmail.com>
2020-12-23 18:49:01 +0300

    feat: add statement to change data

--- src/components/ListCovid/ListCovid.tsx
+++ src/components/ListCovid/ListCovid.tsx
@@ -89,16 +89,54 @@ const tableColumns1 = [
   },
 ] as Column<TestData>[];
 
+const tableColumns2 = [
+  {
+    Header: 'Country',
+    accessor: 'country',
+    width: 70,
+    Cell: ({ row }) => (row.original.country === null ? (
+      '-'
+    ) : (
+      <span className={styles['list-span']}>{row.original.country}</span>
+    )),
+  },
+  {
+    Header: 'Icon',
+    accessor: 'flag',
+    width: 50,
+    Cell: ({ row }) => (row.original.flag === null ? (
+      0
+    ) : (
+      <span className={styles['list-span']}>
+        <img
+          src={`https://www.countryflags.io/${row.original.flag.toLowerCase()}/shiny/32.png`}
+          alt={`flag_${row.original.flag}`}
+        />
+      </span>
+    )),
+  },
+  {
+    Header: 'Recovered',
+    accessor: 'total',
+    width: 80,
+    Cell: ({ row }) => (row.original.total === null ? (
+      0
+    ) : (
+      <span className={styles['list-span']}>{row.original.total}</span>
+    )),
+  },
+] as Column<TestData>[];
+
 const ListCovid = (): JSX.Element => {
   const [stateList, setStateList] = useState<TestData[]>([]);
-  const [stateTest] = useState(0);
+  const [stateTest, setStateTest] = useState(0);
   const [stateHeader, setStateHeader] = useState(tableColumns);
 
   const dataBefore = useCovidMapService();
 
   useEffect(() => {
     if (dataBefore.status === 'loaded') {
-      const obj: TestData[] = [];
+      let obj: TestData[] = [];
 
       for (let i = 0; i < dataBefore.data.length; i += 1) {
         const newobj: TestData[] = [
@@ -111,16 +149,64 @@ const ListCovid = (): JSX.Element => {
         obj.push(...newobj);
       }
 
-      setStateList(obj);
       if (stateTest === 0) {
-        setStateHeader(tableColumns1);
+        setStateHeader(tableColumns);
+        obj = [];
+        for (let i = 0; i < dataBefore.data.length; i += 1) {
+          const newobj: TestData[] = [
+            {
+              country: dataBefore.data[i].country,
+              total: dataBefore.data[i].cases,
+              flag: dataBefore.data[i].countryInfo.iso2,
+            },
+          ];
+          obj.push(...newobj);
+        }
       }
+      if (stateTest === 1) {
+        setStateHeader(tableColumns1);
+        obj = [];
+        for (let i = 0; i < dataBefore.data.length; i += 1) {
+          const newobj: TestData[] = [
+            {
+              country: dataBefore.data[i].country,
+              total: dataBefore.data[i].deaths,
+              flag: dataBefore.data[i].countryInfo.iso2,
+            },
+          ];
+          obj.push(...newobj);
+        }
+      }
+      if (stateTest === 2) {
+        setStateHeader(tableColumns2);
+        obj = [];
+        for (let i = 0; i < dataBefore.data.length; i += 1) {
+          const newobj: TestData[] = [
+            {
+              country: dataBefore.data[i].country,
+              total: dataBefore.data[i].recovered,
+              flag: dataBefore.data[i].countryInfo.iso2,
+            },
+          ];
+          obj.push(...newobj);
+        }
+      }
+      setStateList(obj);
     }
   }, [dataBefore, stateTest]);
 
   return (
     <ComponentLayout>
       <div className={styles['list-covid']}>
+        <button type="button" className={styles['list-button']} onClick={() => setStateTest(1)}>
+          Death
+        </button>
+        <button type="button" className={styles['list-button']} onClick={() => setStateTest(0)}>
+          Total
+        </button>
+        <button type="button" className={styles['list-button']} onClick={() => setStateTest(2)}>
+          Recovered
+        </button>
         <List<TestData> columns={stateHeader} data={stateList} />
       </div>
     </ComponentLayout>
